af57030744fa3b9c7efaee610836af807089c441
Henry Stanley <hstanley@pivotal.io>
2016-11-23 18:35:07 +0000

    Fix wake script

--- wake.sh
+++ wake.sh
@@ -2,5 +2,5 @@
 
 if which VBoxManage; then
   vms=$(VBoxManage list vms | grep bosh-lite | cut -d '"' -f 2)
-  echo "$vms" | xargs -IN VBoxManage controlvm N resume
+  echo "$vms" | xargs -IN VBoxManage startvm N --type headless
 fi
